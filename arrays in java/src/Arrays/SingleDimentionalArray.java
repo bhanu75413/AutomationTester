78ad882b3d712c5102268fb41cb90a9c978fae03
BHANU <BHANU@DESKTOP-H2LB4QG>
2022-11-17 15:34:28 +0530

    bhanu first commit

---
+++ arrays in java/src/Arrays/SingleDimentionalArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class SingleDimentionalArray {
+
+	public static void main(String[] args) {
+		
+		
+      for(int i=17;i>=1;i--) {
+    	  System.out.println(i);
+    	  
+      }
+	}
+
+}
